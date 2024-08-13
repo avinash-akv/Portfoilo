49a27f7fe7612cd851f602765ca0bd527472d6ed
Avinash Kumar Verma <125593008+avinash-akv@users.noreply.github.com>
2024-08-13 23:26:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// Initialize Materialize Components
-document.addEventListener('DOMContentLoaded', function() {
-    const sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js">
+
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    emailjs.sendForm('service_8rxhbyv', 'template_09jrykp', this)
+        .then(function() {
+            alert('Message sent successfully!');
+        }, function(error) {
+            alert('Failed to send message. Please try again later.');
+        });
 });
